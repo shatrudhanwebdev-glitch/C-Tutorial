06abce379ac352facf941a51dc9bdfc3ea98d2f0
Shatrudhan Kumar <shatrudhan.webdev@gmail.com>
2025-08-18 17:11:02 +0530

    Yes

---
+++ Chapter 01/Questions/1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+//shatrudhan.web.developer
+
+int main() {
+
+    int Num1, Num2;
+
+    //Input number from user
+    printf("Enter of value of Num1 : ");
+    scanf("%d", &Num1);
+
+    printf("Enter of value of Num2 : ");
+    scanf("%d", &Num2);
+
+    int sum = Num1 + Num2;
+
+    printf("Valur of sum = %d\n", sum);
+
+    return 0;
+}
