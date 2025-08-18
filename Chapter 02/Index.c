9370ba5b4e2cb7b2f3f921d708801ec56896c0c7
Shatrudhan Kumar <shatrudhan.webdev@gmail.com>
2025-08-18 16:51:30 +0530

    yes

---
+++ Chapter 02/Index.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+//shatrudhan.web.developer
+
+int main() {
+
+    int x, y;
+
+    printf("Enter of value of x : ");
+    scanf("%d", &x);
+
+    printf("Enter of value of y : ");
+    scanf("%d", &y);
+
+    int sum = x + y;
+
+    printf("Valur of sum = %d\n", sum);
+
+    return 0;
+}
