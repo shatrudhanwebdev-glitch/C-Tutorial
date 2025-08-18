a3bda1d9f24b7574056774522f30a50d761bf903
Shatrudhan Kumar <shatrudhan.webdev@gmail.com>
2025-08-18 22:41:11 +0530

    yes

---
+++ Chapter 01/Questions/3.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+//shatrudhan.web.developer
+
+int main() {
+
+    //Area of square
+
+    int side, area;
+
+    //input side from user
+    printf("Enter area of side : ");
+    scanf("%d", &side);
+
+    //formula area of square
+    area = side * side;
+
+    //outpput
+    printf("Value area of square = %d\n", area);
+
+    return 0;
+}
